177aba025258b3daf0e24c3bda68d97b632dc703
Patrick Kranz <patrick-kranz@live.de>
2020-04-03 18:24:25 +0200

    increased resolution

--- AssetGenerator/Program.cs
+++ AssetGenerator/Program.cs
@@ -14,7 +14,7 @@ namespace AssetGenerator
 {
     static class Program
     {
-        private const double Resolution = 4;
+        private const double Resolution = 8;
 
         static async Task Main(string[] args)
         {
